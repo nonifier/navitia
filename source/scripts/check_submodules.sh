6a1800c950edb1608223155328d03b7bdb7cdc98
Nonifier <olivier.georget@kisio.org>
2018-09-05 18:15:37 +0200

    Add to Travis bash script that make sure submodules are merged

---
+++ source/scripts/check_submodules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ev
+
+submodules=`git submodule | tr -s \  : `  # trim the spaces, and convert them to colon
+
+for submod in $submodules
+do
+	submod_len=${#submod}
+	module=${submod:1:$submod_len} # remove the first character that can be a '+' or '-'
+
+	sha1=`echo $module | cut -d: -f1` # Extract submodule Sha1
+	module_path=`echo $module | cut -d: -f2` # Extract submodule path
+
+	head=`cat .git/modules/$module_path/refs/remotes/origin/HEAD` # eg. "ref: refs/remotes/origin/master"
+	remote_ref=`echo $head | sed "s%ref: refs/remotes/%%"` # eg. origin/master
+	
+	pushd $module_path > /dev/null
+		is_merged=`git branch --all --merged HEAD --no-color | grep $remote_ref`
+		contained=`git branch --all --contains HEAD --no-color | grep $remote_ref`
+		if [ -z "$is_merged" -a -z "$contained" ]; then
+		  echo "A submodule points to a commit not merged to its head ! "
+		  echo " > submodule: $module_path"
+		  echo " > commit: $sha1"
+		  echo " > HEAD: $remote_ref"
+		  exit 1
+		fi
+	popd > /dev/null
+done
